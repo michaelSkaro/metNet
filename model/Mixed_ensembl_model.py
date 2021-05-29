db488f863a7ee52a2d38f2652f7759cab27e6ad6
Michael Skaro <mfs61958@uga.edu>
2021-05-29 11:59:49 -0400

    Create Mixed_ensembl_model.py

---
+++ model/Mixed_ensembl_model.py
@@ -0,0 +1,63 @@
+# make classes that expect an input of certain input For DNA, RNA, and methylation
+
+class molecule:
+    def __init__(self, m_type,path):
+        self.m_type = m_type
+        self.path = path
+        self.
+        print("molecule being processed: " + str(m_type))
+        pass
+    def read_cut(self):
+        pass
+    def chunk_it(self):
+        pass
+    def CT(self):
+        pass
+    def labeler(self):
+        pass
+    def build_input(self):
+        pass
+    def feature_selection(self):
+        pass
+    def grade_all(self):
+        pass
+    def join(self):
+        pass
+    def subset(self):
+        pass
+    def encode(self):
+        pass
+    def data_preprocessing(self):
+        pass
+    def balance(self):
+        pass
+    def evaluate(self):
+        pass
+
+
+class Classifier:
+    def build_calssifier(self):
+        pass
+    def split(self):
+        pass
+    def fit_clf(self):
+        pass
+    def visualize(self):
+        pass
+
+class DNA(molecule):
+  pass
+class RNA(molecule):
+  pass
+class methylation(molecule):
+  pass
+
+
+
+#TODO[]
+# []modify the structure to pass to the classes. 
+# []Then the molecule or image will define the methods that are called on that object
+# []implement the constuctors in the molecule class and pass them as abstractions
+#################################################
+#################################################
+#################################################
